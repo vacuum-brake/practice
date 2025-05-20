682be638ddf793db2e518a182c268766d7221b68
vacuum-brake <ellieserpellstevens@gmail.com>
2025-05-20 10:58:40 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,13 +1,13 @@
-import scraperwiki
-html = scraperwiki.scrape('https://web.archive.org/web/20120318184750/http://www.inmo.ie/6022')
+# import scraperwiki
+# html = scraperwiki.scrape('https://web.archive.org/web/20120318184750/http://www.inmo.ie/6022')
 print "Click on the ...more link to see the whole page"
-print html
-import lxml.html
-root = lxml.html.fromstring(html)
-tds = root.cssselect('td')
-for td in tds:
-  print lxml.html.tostring(td)
-  print td.text
-for td in tds:
-  record = { "td" : td.text }
-  scraperwiki.sqlite.save(["td"], record)
+# print html
+# import lxml.html
+# root = lxml.html.fromstring(html)
+# tds = root.cssselect('td')
+# for td in tds:
+#   print lxml.html.tostring(td)
+#   print td.text
+# for td in tds:
+#   record = { "td" : td.text }
+#   scraperwiki.sqlite.save(["td"], record)
