79eb8b0ea4023f6521b5e11c8c78939294777118
vacuum-brake <ellieserpellstevens@gmail.com>
2025-05-20 10:19:29 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,13 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+import scraperwiki
+html = scraperwiki.scrape('https://web.archive.org/web/20120318184750/http://www.inmo.ie/6022')
+print "Click on the ...more link to see the whole page"
+print html
+import lxml.html
+root = lxml.html.fromstring(html)
+tds = root.cssselect('td')
+for td in tds:
+  print lxml.html.tostring(td)
+  print td.text
+for td in tds:
+  record = { "td" : td.text }
+  scraperwiki.sqlite.save(["td"], record)
